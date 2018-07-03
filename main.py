a302899d8d60960867b0fddd1cef90321c3d455d
Hidehiko Nagashiba <naga@jmas.co.jp>
2018-07-03 08:01:53 +0000

    var display

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  s = "abc"
+  lis = ["a1", "a2", "a3"]
+  dic = {"name":"John", "age":24}
+  bl = True
+
+  return render_template('index.html', s=s, lis=lis, dic=dic, bl=bl)
 
 if __name__ == '__main__':
   app.run()
