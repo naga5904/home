f39bc3f372d8f32bf79c58040c99a6a999963653
Hidehiko Nagashiba <naga@jmas.co.jp>
2018-07-03 07:07:20 +0000

    index.html display

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-  return 'Hello, Azure   in!'
+def index():
+  return render_template('index.html')
 
 if __name__ == '__main__':
   app.run()
