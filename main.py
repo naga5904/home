e085174ae913ef2b9ffc0358d7eb499bd9d9c75e
Hidehiko Nagashiba <naga@jmas.co.jp>
2018-07-03 08:11:02 +0000

    request

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  s = "abc"
-  lis = ["a1", "a2", "a3"]
-  dic = {"name":"John", "age":24}
-  bl = True
+    return render_template('index.html')
 
-  return render_template('index.html', s=s, lis=lis, dic=dic, bl=bl)
+
+@app.route('/test', methods=['GET', 'POST'])
+def test():
+    if request.method == 'GET':
+        res = request.args.get('get_value')
+    elif request.method == 'POST':
+        res = request.form['post_value']
+
+    return res
 
 if __name__ == '__main__':
-  app.run()
+    app.run()
+
